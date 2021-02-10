25381b02cec72da81475a7e0bd8b37c3ebc84106
Carlos Bruno <cbcarlos08@gmail.com>
2021-02-10 16:42:25 -0400

    -

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import server from './server'
 import { env } from 'process'
 
-const PORT = env.SERVER_PORT || 80
+const PORT = env.PORT || 3006
 
 server.listen( '0.0.0.0',PORT, () =>{
     console.log(`API rodando da porta ${PORT}` );
