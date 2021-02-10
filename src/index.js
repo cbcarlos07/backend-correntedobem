9994e7c0eb1a285732958444cc0a5bdd5fe17af2
Carlos Bruno <cbcarlos08@gmail.com>
2021-02-10 16:30:44 -0400

    Correcao

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import server from './server'
 import { env } from 'process'
 
 
-const PORT = env.SERVER_PORT
+const PORT = env.SERVER_PORT || 80
 
 server.listen( PORT, () =>{
     console.log(`API rodando da porta ${PORT}` );
