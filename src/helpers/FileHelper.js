4fb7fdd76183526921ac2315fa7802ed82ea9621
Carlos Bruno <cbcarlos08@gmail.com>
2021-02-12 14:46:49 -0400

    Path

--- src/helpers/FileHelper.js
+++ src/helpers/FileHelper.js
@@ -3,13 +3,13 @@ import path from 'path'
 export default {
     convertToImg(filename, base64File){
         const base64Data = base64File.replace(/^data:image\/png;base64,/,"")
-        let local = path.resolve('public',`${filename}`)
+        let local = path.resolve('./public',`${filename}`)
         fs.writeFile(local, base64Data, 'base64', (err)=>{
             console.log(err);
         })
     },
     remove(filename){
-        let local = path.resolve('public',`${filename}`)
+        let local = path.resolve('./public',`${filename}`)
         try {
             fs.unlinkSync(local)
         } catch (error) {
