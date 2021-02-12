8b21a66d117fd27676ef2b9ea7300ada457b816a
Carlos Bruno <cbcarlos08@gmail.com>
2021-02-12 15:58:53 -0400

    Path

--- src/service/TemaService.js
+++ src/service/TemaService.js
@@ -5,12 +5,26 @@ class TemaService{
 
     static create(obj) {
         return new Promise((resolve, reject)=>{
-            let logo         = obj.logo
-            let logoNameFile = `${shortId.generate()}.png`
-            obj.logo         = logoNameFile
+            let logo =''
+            let logoNameFile = ''
+            let image = ''
+            let imgNameFile = ''
+            if( obj.logo ){
+                logo         = obj.logo
+                logoNameFile = `${shortId.generate()}.png`
+                obj.logo         = logoNameFile
+            }
+
+            if( obj.image ){
+                image         = obj.logo
+                imgNameFile = `${shortId.generate()}.png`
+                obj.image         = imgNameFile
+            }
+
             TemaRepository.create( obj )
                 .then(response => {
-                    FileHelper.convertToImg( logoNameFile, logo )
+                    if(obj.logo)  FileHelper.convertToImg( logoNameFile, logo )
+                    if(obj.image)  FileHelper.convertToImg( logoNameFile, image )
                     resolve(response)
                 })
             
@@ -21,17 +35,32 @@ class TemaService{
        return new Promise(async(resolve, reject)=>{
             let logo = ''
             let logoNameFile = ''
+            let image = ''
+            let imgNameFile = ''
+            let objTema = await this.findByPK( id )
             if(obj.logo){
-                let objTema = await this.findByPK( id )
-                FileHelper.remove( objTema.logo )
+                if( objTema.logo ){
+                    FileHelper.remove( objTema.logo )
+                }
                 logo         = obj.logo
                 logoNameFile = `${shortId.generate()}.png`
                 obj.logo= logoNameFile
                 
            }
+            if( obj.image ){
+                if(objTema.image){
+                    FileHelper.remove( objTema.logo )
+                    
+                }
+                image         = obj.image
+                imgNameFile = `${shortId.generate()}.png`
+                obj.image         = imgNameFile
+            }
+            console.log('obj editar',obj);
            TemaRepository.update( id, obj )
             .then(response =>{
                 if(obj.logo) FileHelper.convertToImg( logoNameFile, logo )
+                if(obj.image) FileHelper.convertToImg( imgNameFile, image )
                 resolve( response )
             })
 
